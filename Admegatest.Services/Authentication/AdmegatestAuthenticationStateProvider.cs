85629a779ed2d1128061b6660cb3396faab5d148
Tomás Agú <69135623+agu99tomas@users.noreply.github.com>
2021-12-22 00:01:24 -0300

    feature: add new package

--- Admegatest.Services/Authentication/AdmegatestAuthenticationStateProvider.cs
+++ Admegatest.Services/Authentication/AdmegatestAuthenticationStateProvider.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Admegatest.Services.Authentication
 {
     public class AdmegatestAuthenticationStateProvider : AuthenticationStateProvider
     {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
