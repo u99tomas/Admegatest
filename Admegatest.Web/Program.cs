791355bd61215e74162ea2a0c74726293ad37791
Tomás Agú <69135623+agu99tomas@users.noreply.github.com>
2021-12-22 00:05:34 -0300

    feature: add service custom authentication

--- Admegatest.Web/Program.cs
+++ Admegatest.Web/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Admegatest.Services.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+// Custom Authentication
+builder.Services.AddScoped<AuthenticationStateProvider, AdmegatestAuthenticationStateProvider>();
 // MudBlazor
 builder.Services.AddMudServices();
 
