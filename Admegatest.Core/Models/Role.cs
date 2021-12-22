fd3f86e7a2a0087be40ad06bee067b505c25187e
Tomás Agú <69135623+agu99tomas@users.noreply.github.com>
2021-12-21 23:32:49 -0300

    feature: User and Role class

---
+++ Admegatest.Core/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admegatest.Core.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleDescription { get; set; }
+    }
+}
