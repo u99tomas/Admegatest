411ece8b70920a4fe8e90c8154ec9b3ad3cff2c6
Tomás Agú <69135623+agu99tomas@users.noreply.github.com>
2021-12-25 14:48:13 -0300

    feature: mgirations

--- Admegatest.Core/Models/AuthenticationAndAuthorization/RefreshToken.cs
+++ Admegatest.Core/Models/AuthenticationAndAuthorization/RefreshToken.cs
@@ -8,7 +8,7 @@ namespace Admegatest.Core.Models.AuthenticationAndAuthorization
 {
     public class RefreshToken
     {
-        public int TokenId { get; set; }
+        public int RefreshTokenId { get; set; }
         public int UserId { get; set; }
         public string Token { get; set; }
         public virtual User User { get; set; }
