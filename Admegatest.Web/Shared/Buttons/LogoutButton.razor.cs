56964751ebbc1c82682016ee0a0cebc913baa845
Tomás Agú <69135623+agu99tomas@users.noreply.github.com>
2021-12-28 08:09:35 -0300

    refactor

--- Admegatest.Web/Shared/Buttons/LogoutButton.razor.cs
+++ Admegatest.Web/Shared/Buttons/LogoutButton.razor.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 
-namespace Admegatest.Web.Shared.Components.Buttons
+namespace Admegatest.Web.Shared.Buttons
 {
     public partial class LogoutButton
     {
--- Admegatest.Web/Shared/Useful/RedirectToLogin.razor.cs
+++ Admegatest.Web/Shared/Useful/RedirectToLogin.razor.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-namespace Admegatest.Web.Shared.Components.Useful
+namespace Admegatest.Web.Shared.Useful
 {
     public partial class RedirectToLogin
     {
