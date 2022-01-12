cd36d70e542ac17848d3b51cf783e19c6a00f000
Tomás <agu99tomas@gmail.com>
2022-01-12 01:18:18 -0300

    migrations

--- src/Backend/Admegatest/Admegatest.Core/Models/Account/UserRoles.cs
+++ src/Backend/Admegatest/Admegatest.Core/Models/Account/UserRoles.cs
@@ -2,6 +2,7 @@
 {
     public class UserRoles
     {
+        public int Id { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
         public int RoleId { get; set; }
