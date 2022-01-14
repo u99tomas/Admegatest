dc33f0e62260a65cb945bdf134fb6992b1431179
Tomás <agu99tomas@gmail.com>
2022-01-14 00:11:18 -0300

    refactor

--- src/UI/Admegatest/Admegatest.Web/Pages/Account/Login.razor.cs
+++ src/UI/Admegatest/Admegatest.Web/Pages/Account/Login.razor.cs
@@ -39,7 +39,7 @@ namespace Admegatest.Web.Pages.Account
             }
         }
 
-        private async void ValidateUser()
+        private async Task ValidateUser()
         {
             Errors.Clear();
             ShowLoadingButton();
