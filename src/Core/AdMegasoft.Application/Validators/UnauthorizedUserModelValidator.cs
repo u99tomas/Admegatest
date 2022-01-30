a4e77123ef752f21bdbf63c686a7b561e119c364
Tomás <agu99tomas@gmail.com>
2022-01-30 02:15:56 -0300

    fix

--- src/Core/AdMegasoft.Application/Validators/UnauthorizedUserModelValidator.cs
+++ src/Core/AdMegasoft.Application/Validators/UnauthorizedUserModelValidator.cs
@@ -15,13 +15,5 @@ namespace AdMegasoft.Application.Validators
                 .NotEmpty()
                 .WithMessage("¡Se requiere contraseña!");
         }
-
-        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
-        {
-            var result = await ValidateAsync(ValidationContext<UnauthorizedUserModel>.CreateWithOptions((UnauthorizedUserModel)model, x => x.IncludeProperties(propertyName)));
-            if (result.IsValid)
-                return Array.Empty<string>();
-            return result.Errors.Select(e => e.ErrorMessage);
-        };
     }
 }
