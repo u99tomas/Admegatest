9c6997076491eff216b3db45a86bda84ce7b9f6e
Tomás <agu99tomas@gmail.com>
2022-01-30 03:50:40 -0300

    user entity configuration

---
+++ src/Infrastructure/AdMegasoft.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AdMegasoft.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AdMegasoft.Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(u => u.Name)
+                .HasMaxLength(25)
+                .IsRequired();
+
+            builder.HasIndex(u => u.Name)
+                .IsUnique();
+
+            builder.Property(u => u.Password)
+                .IsRequired();
+        }
+    }
+}
--- src/Infrastructure/AdMegasoft.Infrastructure/Persistence/Migrations/20220130064850_UserConfig.Designer.cs
+++ src/Infrastructure/AdMegasoft.Infrastructure/Persistence/Migrations/20220130064850_UserConfig.Designer.cs
@@ -3,6 +3,7 @@ using AdMegasoft.Infrastructure.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AdMegasoft.Infrastructure.persistence.Migrations
 {
     [DbContext(typeof(AdMegasoftDbContext))]
-    partial class AdMegasoftDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220130064850_UserConfig")]
+    partial class UserConfig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
