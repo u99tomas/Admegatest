0104dd1d8a04dc4d5cf83d459086f097252dc3f1
Tomás <agu99tomas@gmail.com>
2022-02-01 00:11:57 -0300

    add edit role command

---
+++ src/Core/AdMegasoft.Application/Features/Roles/Commands/AddEdit/AddEditRoleCommand.cs
@@ -0,0 +1,30 @@
+﻿using AdMegasoft.Application.Interfaces.Repositories;
+using AdMegasoft.Application.Mappings;
+using MediatR;
+
+namespace AdMegasoft.Application.Features.Roles.Commands.AddEdit
+{
+    public class AddEditRoleCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    internal class AddEditRoleCommandHandler : IRequestHandler<AddEditRoleCommand, int>
+    {
+        private readonly IRoleRepository _roleRepository;
+
+        public AddEditRoleCommandHandler(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public async Task<int> Handle(AddEditRoleCommand request, CancellationToken cancellationToken)
+        {
+            var role = request.ToRole();
+            await _roleRepository.AddOrUpdateAsync(role);
+            return role.Id;
+        }
+    }
+}
---
+++ src/Core/AdMegasoft.Application/Mappings/AddEditRoleCommandMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using AdMegasoft.Application.Features.Roles.Commands.AddEdit;
+using AdMegasoft.Domain.Entities;
+
+namespace AdMegasoft.Application.Mappings
+{
+    public static class AddEditRoleCommandMappingExtensions
+    {
+        public static Role ToRole(this AddEditRoleCommand addEditRoleCommand)
+        {
+            return new Role
+            {
+                Id = addEditRoleCommand.Id,
+                Name = addEditRoleCommand.Name,
+                Description = addEditRoleCommand.Description,
+            };
+        }
+    }
+}
