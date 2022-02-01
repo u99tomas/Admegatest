e9ce02a8f12808402e02d4b6cc6780de6918036d
Tomás <agu99tomas@gmail.com>
2022-02-01 10:46:14 -0300

    refactor

--- src/Core/AdMegasoft.Abstractions/Abstractions/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using AdMegasoft.Domain.Entities;
-
-namespace AdMegasoft.Abstractions.Abstractions
-{
-    public interface IUserRepository : IGenericRepository<User>
-    {
-        Task<User?> GetActiveUserByPasswordNameAsync(string name, string password);
-    }
-}
