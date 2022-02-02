1580518e5275463eff79fe5f1a4989303b38ce4a
Tomás <agu99tomas@gmail.com>
2022-02-02 00:51:04 -0300

    refactor

--- src/Shared/AdMegasoft.Shared/Constants/Permission/Permissions.cs
+++ src/Shared/AdMegasoft.Shared/Constants/Permission/Permissions.cs
@@ -7,7 +7,6 @@
             public const string View = "Permisos.Usuarios.Ver";
             public const string Create = "Permisos.Usuarios.Crear";
             public const string Edit = "Permisos.Usuarios.Editar";
-            public const string Search = "Permisos.Usuarios.Buscar";
         }
 
         public static class Roles
@@ -15,7 +14,6 @@
             public const string View = "Permisos.Roles.Ver";
             public const string Create = "Permisos.Roles.Crear";
             public const string Edit = "Permisos.Roles.Editar";
-            public const string Search = "Permisos.Roles.Buscar";
         }
     }
 }
