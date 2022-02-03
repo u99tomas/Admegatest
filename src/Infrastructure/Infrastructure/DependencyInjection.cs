8855d6bbf71d2697d2b1921402a58adb9738d4eb
Tomás <agu99tomas@gmail.com>
2022-02-03 02:57:58 -0300

    dependency injection

--- src/Infrastructure/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/Infrastructure/DependencyInjection.cs
@@ -9,23 +9,25 @@ namespace Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddAdMegasoftInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            #region Database Connection
+            #region Persistence
             services.AddDbContext<ApplicationDbContext>(
                 options => options.UseSqlServer(
                     configuration.GetConnectionString("AdMegasoftDb"))
             );
             #endregion
 
+            #region Services
+            #endregion
+
             #region Repositories
             services.AddTransient(typeof(IRepositoryAsync<,>), typeof(RepositoryAsync<,>));
             services.AddTransient(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
-
-            //services.AddTransient(IUserRepository, UserRepository);
-            #endregion
-
-            #region Services
+            services.AddTransient<IRoleRepository, RoleRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IRoleRepository, RoleRepository>();
+            services.AddTransient<IPermissionRepository, PermissionRepository>();
             #endregion
 
             return services;
