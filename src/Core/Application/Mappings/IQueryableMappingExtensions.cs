7b96d75b1b11c00f5db675eff54ec41eacddd6be
Tomás <agu99tomas@gmail.com>
2022-02-05 23:40:57 -0300

    fix

--- src/Core/Application/Mappings/IQueryableMappingExtensions.cs
+++ src/Core/Application/Mappings/IQueryableMappingExtensions.cs
@@ -8,12 +8,12 @@ namespace Application.Mappings
     {
         public static async Task<PagedResponse<T>> ToPagedResponseAsync<T>(this IQueryable<T> queryable, int page, int pageSize)
         {
+            var totalItems = await queryable.CountAsync();
+
             var items = await queryable
                 .Skip(page * pageSize)
                 .Take(pageSize)
                 .ToListAsync();
-
-            var totalItems = await queryable.CountAsync();
 
             return new PagedResponse<T>
             {
