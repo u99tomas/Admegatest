926c1a0511f9a77c59f5957743ccc43d7e09d5d7
Tomás <agu99tomas@gmail.com>
2022-02-13 02:35:52 -0300

    Result wrapper

---
+++ src/Core/Application/Wrappers/Result.cs
@@ -0,0 +1,43 @@
+﻿namespace Application.Wrappers
+{
+    public class Result<T>
+    {
+        public T Data { get; set; }
+        public bool Succeeded { get; set; }
+        public string[] Messages { get; set; }
+
+        public Result(bool succeeded, IEnumerable<string> messages, T data)
+        {
+            Succeeded = succeeded;
+            Messages = messages.ToArray();
+            Data = data;
+        }
+
+        public Result(bool succeeded, IEnumerable<string> messages)
+        {
+            Succeeded = succeeded;
+            Messages = messages.ToArray();
+        }
+
+        public Result(bool succeeded, T data)
+        {
+            Succeeded = succeeded;
+            Data = data;
+        }
+
+        public static Result<T> Success(IEnumerable<string> messages, T data)
+        {
+            return new Result<T>(true, messages, data);
+        }
+
+        public static Result<T> Success(T data)
+        {
+            return new Result<T>(true, data);
+        }
+
+        public static Result<T> Failure(IEnumerable<string> messages)
+        {
+            return new Result<T>(false, messages);
+        }
+    }
+}
