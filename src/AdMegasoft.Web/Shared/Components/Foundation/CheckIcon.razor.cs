03c6379dfc6f4c04586a81895501126118428d4d
Tomás <agu99tomas@gmail.com>
2022-03-02 01:31:05 -0300

    refactor

---
+++ src/AdMegasoft.Web/Shared/Components/Foundation/CheckIcon.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace AdMegasoft.Web.Shared.Components.Foundation
+{
+    public partial class CheckIcon
+    {
+        [Parameter]
+        public bool Checked { get; set; }
+
+        private string _icon
+        {
+            get
+            {
+                return Checked ? Icons.Material.Filled.CheckCircle : Icons.Material.Filled.Cancel;
+            }
+        }
+
+        private Color _color
+        {
+            get
+            {
+                return Checked ? Color.Success : Color.Error;
+            }
+        }
+    }
+}
